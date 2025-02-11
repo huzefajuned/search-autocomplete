b2c3286edd26d71c965cdde73ef1f2334f799cc7
huzefa <huzefaghurna@gmail.com>
2025-02-11 18:47:03 +0530

    feat : search api add

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,88 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { ChangeEvent, useEffect, useState } from "react";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [loading, setLoading] = useState<boolean>(false);
+  const [input, setInput] = useState<string>("");
+  const [data, setData] = useState<[] | null>([]);
+  const search_API = `https://dummyjson.com/products/search?q=${input}`;
+
+  const handleOnChange = (e: ChangeEvent<HTMLInputElement>) => {
+    console.log("handleOnChange hit!");
+    setInput(e.target.value);
+  };
+
+  // fetch the api function
+  const fetchData = () => {
+    if (input === "") return setData([]);
+    setLoading(true);
+    console.log(" input", input);
+    console.log("useEffect Hit!");
+    fetch(search_API)
+      .then((res) => res.json())
+      .then((data) => {
+        console.log("api hit!");
+        // store only titles in an array!
+        const onlyTitles = data.products.map(
+          (product: { title: string }) => product.title
+        );
+        setData(onlyTitles);
+      })
+      .catch((err) => console.log("err in api", err))
+      .finally(() => setLoading(false));
+  };
+
+  useEffect(() => {
+    fetchData();
+  }, [input]);
+
+  console.log(" data", data);
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div className=" font-mono p-2 h-screen w-screen flex flex-col justify-start gap-10  items-center  bg-gray-200">
+      {/* input */}
+      <div className="flex flex-row items-center relative pt-20 pb-10">
+        <input
+          onChange={(e) => handleOnChange(e)}
+          type="text"
+          placeholder="Enter your query!"
+          value={input}
+          className="outline-none  border-gray-300 bg-red-50 p-4 border-2 active:outline-red-200   text-xl shadow-2xl  rounded-xl text-black font-normal  placeholder:font-normal placeholder-gray-500  placeholder:text-lg placeholder:space-x-0.5 w-full sm:w-xl  sm:h-13  md:w-2xl md:h-14"
+        />
+        {/* loading UI , when api is calling */}
+
+        <img
+          className={`${
+            loading ? "flex" : "hidden"
+          }   w-10 h-10 bg-transparent  absolute right-2   rounded-full border-2 border-gray-300  `}
+          src="https://imgs.search.brave.com/hKTP_TW7g9L-0PEM2uSrilfQJUX-Z60wKsL0jiJWqFg/rs:fit:860:0:0:0/g:ce/aHR0cHM6Ly9tZWRp/YTAuZ2lwaHkuY29t/L21lZGlhL3hUazla/dk1uYklpSWV3N0lw/Vy9naXBoeS5naWY_/Y2lkPTc5MGI3NjEx/ZTZ2ZXFqNGxwMTcy/aDFmYjR0dXBkZHJy/ODdhaWlhNXc4Z3ln/NnRhbSZlcD12MV9n/aWZzX3NlYXJjaCZy/aWQ9Z2lwaHkuZ2lm/JmN0PWc.gif"
+          alt=""
+        />
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+
+      {/* results */}
+      {data?.length !== 0 && (
+        <div className="bg-gray-100 max-h-[40%] w-[40%] flex flex-col gap-2 rounded-lg p-2 overflow-scroll">
+          {data?.map((title: string, i: number) => {
+            return (
+              <div
+                key={i}
+                className="bg-gray-400 py-2 px-1 cursor-pointer hover:bg-white rounded-4xl font-bold text-amber-200 hover:text-black border-2"
+              >
+                <h2>{title}</h2>
+              </div>
+            );
+          })}
+        </div>
+      )}
+      {/* when query & no data result is available */}
+      {input !== "" && data?.length === 0 && !loading && (
+        <div className="bg-amber-50 rounded-2xl p-2">
+          <h2 className="text-3xl p-2">Sorry, No matching Query found!</h2>
+        </div>
+      )}
+    </div>
+  );
 }
 
-export default App
+export default App;
